a6eee5e5114c38afd272d5b0d9bbcc48434ec55e
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-01-29 20:54:14 -0800

    Filter by department and age

--- kiewit/src/App.js
+++ kiewit/src/App.js
@@ -6,7 +6,7 @@ import TableLayout from './components/TableLayout';
 function App() {
   const [data, setData] = useState(employeeData);
   const [q, setQ] = useState('');
-
+  const [searchColumns, setSearchColumns] = useState(["name", "department"])
 
   // useEffect(() => {
   //   fetch("endpoint")
@@ -17,16 +17,32 @@ function App() {
   // Search: indexOf() or includes()
   // search by name, department, age(toString())
   function search(rows) {
-    const columns = rows[0] && Object.keys(rows[0])
+    // const columns = rows[0] && Object.keys(rows[0])
     return rows.filter(row =>
-      columns.some((column) => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1))
+      searchColumns.some((column) => row[column].toString().toLowerCase().indexOf(q.toLowerCase()) > -1))
   }
 
-
+  const columns = data[0] && Object.keys(data[0])
   return (
     <div className="App">
       <div>
-        <input type='text' value={q} onChange={(e) => setQ(e.target.value)} />
+        <input type='text' value={q} placeholder="Search.." onChange={(e) => setQ(e.target.value)} />
+        {
+          columns && columns.map(column => (<label>
+            <input type="checkbox" checked={searchColumns.includes(column)}
+              onChange={(e) => {
+                const checked = searchColumns.includes(column)
+                setSearchColumns((prev) =>
+                checked
+                  ? prev.filter((sc) => sc !== column)
+                  : [...prev, column]
+                );
+              }}
+            />
+            {column}
+            </label>
+          ))
+        }
       </div>
       <TableLayout data={search(data)}/>
     </div>
